1035d461e56f03bfc4adfddd674c210b2d17d45d
Gabrielle McCormack <gmccorma@stevens.edu>
2018-04-15 18:58:35 -0400

    Added divs to creating each question so that the elements can be contained

--- gm_test_application.js
+++ gm_test_application.js
@@ -18,31 +18,89 @@ function addQuestionName() {
 
 function addMultipleChoice() {
 	alert('Will add code to append appropriate elements to create a new multiple choice question.  i.e. add label with question name and ordered list with answer choices.');
-
+	var div_mc = document.createElement("div");
+	div_mc.style.width = "100px";
+	div_mc.style.height = "100px";
+	div_mc.style.padding = "15px";
+	div_mc.style.margin = "15px";
+	div_mc.style.background = "red";
+	div_mc.style.color = "white";
+	div_mc.innerHTML = "Multiple Choice Question";
+	document.body.appendChild(div_mc);
+	div_mc.id = "mult_div";
+	//var mcb = document.createElement("button");
+	//div_mc.appendChild(mcb);
 };
 
 function addTrueFalse() {
 	alert('Will add code to append appropriate elements to create a new true/false question.  i.e. add label with question name and true/false answer choices.');
-
+	var div_tf = document.createElement("div");
+	div_tf.style.width = "100px";
+	div_tf.style.height = "100px";
+	div_tf.style.padding = "15px";
+	div_tf.style.margin = "15px";
+	div_tf.style.background = "green";
+	div_tf.style.color = "white";
+	div_tf.innerHTML = "True/False Question";
+	document.body.appendChild(div_tf);
+	div_tf.id = "tf_div";
 };
 
 function addShortAnswer() {
 	alert('Will add code to append appropriate elements to create a new short answer question.  i.e. add textarea and label with question name.');
+	var div_sa = document.createElement("div");
+	div_sa.style.width = "100px";
+	div_sa.style.height = "100px";
+	div_sa.style.padding = "15px";
+	div_sa.style.margin = "15px";
+	div_sa.style.background = "blue";
+	div_sa.style.color = "white";
+	div_sa.innerHTML = "Short Answer Question";
+	document.body.appendChild(div_sa);
+	div_sa.id = "srt_ans_div";
 };
 
 function addEssayAnswer() {
 	alert('Will add code to append appropriate elements to create a new essay answer question.  i.e. add textarea and label with question name.');
-}
+	var div_ea = document.createElement("div");
+	div_ea.style.width = "100px";
+	div_ea.style.height = "100px";
+	div_ea.style.padding = "15px";
+	div_ea.style.margin = "15px";
+	div_ea.style.background = "pink";
+	div_ea.style.color = "white";
+	div_ea.innerHTML = "Essay Answer Question";
+	document.body.appendChild(div_ea);
+	div_ea.id = "ess_ans_div";
+};
 
 function addMatching() {
 	alert('Will add code to append appropriate elements to create a new matching question.  i.e. add label with question name and maybe dropdowns for matching.');
-
-}
+	var div_mt = document.createElement("div");
+	div_mt.style.width = "100px";
+	div_mt.style.height = "100px";
+	div_mt.style.padding = "15px";
+	div_mt.style.margin = "15px";
+	div_mt.style.background = "orange";
+	div_mt.style.color = "white";
+	div_mt.innerHTML = "Matching Question";
+	document.body.appendChild(div_mt);
+	div_mt.id = "match_div";
+};
 
 function addRanking() {
 	alert('Will add code to append appropriate elements to create a new ranking question.  i.e. add label with question name and maybe dropdowns for ranking.');
-
-}
+	var div_rk = document.createElement("div");
+	div_rk.style.width = "100px";
+	div_rk.style.height = "100px";
+	div_rk.style.padding = "15px";
+	div_rk.style.margin = "15px";
+	div_rk.style.background = "purple";
+	div_rk.style.color = "white";
+	div_rk.innerHTML = "Ranking Question";
+	document.body.appendChild(div_rk);
+	div_rk.id = "rank_div";
+};
 	
 // saves data to firebase
 function saveToList(event, data) {
