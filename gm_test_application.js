383070e5e53b5ed686c1df52666075cf9f4dc082
Gabrielle McCormack <gmccorma@stevens.edu>
2018-04-18 11:14:30 -0400

    Added content for creating true/false, essay, and short answer questions

--- gm_test_application.js
+++ gm_test_application.js
@@ -28,50 +28,157 @@ function addMultipleChoice() {
 	div_mc.innerHTML = "Multiple Choice Question";
 	document.body.appendChild(div_mc);
 	div_mc.id = "mult_div";
-	//var mcb = document.createElement("button");
-	//div_mc.appendChild(mcb);
+	var mcb = document.createElement("button");
+	div_mc.appendChild(mcb);
 };
 
 function addTrueFalse() {
 	alert('Will add code to append appropriate elements to create a new true/false question.  i.e. add label with question name and true/false answer choices.');
 	var div_tf = document.createElement("div");
-	div_tf.style.width = "100px";
-	div_tf.style.height = "100px";
+	div_tf.style.width = window.innerWidth;
+	div_tf.style.height = "200px";
 	div_tf.style.padding = "15px";
 	div_tf.style.margin = "15px";
 	div_tf.style.background = "green";
 	div_tf.style.color = "white";
-	div_tf.innerHTML = "True/False Question";
 	document.body.appendChild(div_tf);
 	div_tf.id = "tf_div";
+	
+	// prompt to create true or false question
+	var lbl_1 = document.createElement("label");
+	var txt_1 = document.createTextNode("Enter true or false question.");
+	lbl_1.appendChild(txt_1);
+	document.getElementById("tf_div").appendChild(lbl_1);
+	
+	// insert space between label and input	
+	var br = document.createElement("br");
+	document.getElementById("tf_div").appendChild(br);
+	
+	var ipt_1 = document.createElement("input");
+	ipt_1.style.margin = "15px";
+	document.getElementById("tf_div").appendChild(ipt_1);
+	ipt_1.id = "ipt_1_id";
+	
+	// insert space between label and input	
+	br = document.createElement("br");
+	document.getElementById("tf_div").appendChild(br);
+	br = document.createElement("br");
+	document.getElementById("tf_div").appendChild(br);
+	br = document.createElement("br");
+	document.getElementById("tf_div").appendChild(br);
+		
+	
+	// ********************  NOTE  ***********************
+	// use for tests to designate correct answer, delete for survey version
+	var lbl_2 = document.createElement("label");
+	var txt_2 = document.createTextNode("Select correct answer for question.");
+	lbl_2.appendChild(txt_2);
+	document.getElementById("tf_div").appendChild(lbl_2);
+	
+	// insert space between label and buttons	
+	br = document.createElement("br");
+	document.getElementById("tf_div").appendChild(br);
+	
+	// insert button label (true)
+	var btn_t = document.createElement("button");
+	var txt_t = document.createTextNode("True");
+	btn_t.appendChild(txt_t);
+	document.getElementById("tf_div").appendChild(btn_t);
+	btn_t.style.margin = "15px";
+	btn_t.style.backgroundColor = "rgb(66, 134, 244)";
+	btn_t.id = "t_btn";
+	
+	// insert button label (false)
+	var btn_f = document.createElement("button");
+	var txt_f = document.createTextNode("False");
+	btn_f.appendChild(txt_f);
+	document.getElementById("tf_div").appendChild(btn_f);
+	btn_f.style.margin = "15px";
+	btn_f.style.backgroundColor = "rgb(66, 134, 244)";
+	btn_f.id = "f_btn";
+	
+	// add event listener for when the true or false buttos are selected.  
+	// When selected, button correct answer will be added to firebase (TO DO)
+	//document.getElementById("t_btn").addEventListener("click", buttonSelected("t_btn"));
+	//document.getElementById("f_btn").addEventListener("click", buttonSelected("f_btn"));
+	
+	//  **************** END NOTE *******************
+
+
 };
+
+/*function buttonSelected(id) {
+	var background = document.getElementById(id).style.backgroundColor;
+    if (background == "rgb(66, 134, 244)") {
+        document.getElementById(id).style.backgroundColor = "rgb(86,86,86)";
+    } else {
+        document.getElementById(id).style.backgroundColor = "rgb(66, 134, 244)";
+    }
+};*/
+	
 
 function addShortAnswer() {
 	alert('Will add code to append appropriate elements to create a new short answer question.  i.e. add textarea and label with question name.');
 	var div_sa = document.createElement("div");
-	div_sa.style.width = "100px";
+	div_sa.style.width = window.innerWidth;
 	div_sa.style.height = "100px";
 	div_sa.style.padding = "15px";
 	div_sa.style.margin = "15px";
 	div_sa.style.background = "blue";
 	div_sa.style.color = "white";
-	div_sa.innerHTML = "Short Answer Question";
 	document.body.appendChild(div_sa);
 	div_sa.id = "srt_ans_div";
+	
+	// prompt to create short answer question
+	var lbl_1 = document.createElement("label");
+	var txt_1 = document.createTextNode("Enter short answer question.");
+	lbl_1.appendChild(txt_1);
+	document.getElementById("srt_ans_div").appendChild(lbl_1);
+	
+	// insert space between label and input	
+	var br = document.createElement("br");
+	document.getElementById("srt_ans_div").appendChild(br);
+	
+	var ipt_1 = document.createElement("input");
+	ipt_1.style.margin = "15px";
+	document.getElementById("srt_ans_div").appendChild(ipt_1);
+	ipt_1.id = "ipt_1_id";
+	
+	// insert space between label and input	
+	br = document.createElement("br");
+	document.getElementById("srt_ans_div").appendChild(br);
 };
 
 function addEssayAnswer() {
 	alert('Will add code to append appropriate elements to create a new essay answer question.  i.e. add textarea and label with question name.');
 	var div_ea = document.createElement("div");
-	div_ea.style.width = "100px";
+	div_ea.style.width = window.innerWidth;
 	div_ea.style.height = "100px";
 	div_ea.style.padding = "15px";
 	div_ea.style.margin = "15px";
 	div_ea.style.background = "pink";
 	div_ea.style.color = "white";
-	div_ea.innerHTML = "Essay Answer Question";
 	document.body.appendChild(div_ea);
 	div_ea.id = "ess_ans_div";
+	
+	// prompt to create short answer question
+	var lbl_1 = document.createElement("label");
+	var txt_1 = document.createTextNode("Enter essay answer question.");
+	lbl_1.appendChild(txt_1);
+	document.getElementById("ess_ans_div").appendChild(lbl_1);
+	
+	// insert space between label and input	
+	var br = document.createElement("br");
+	document.getElementById("ess_ans_div").appendChild(br);
+	
+	var ipt_1 = document.createElement("input");
+	ipt_1.style.margin = "15px";
+	document.getElementById("ess_ans_div").appendChild(ipt_1);
+	ipt_1.id = "ipt_1_id";
+	
+	// insert space between label and input	
+	br = document.createElement("br");
+	document.getElementById("ess_ans_div").appendChild(br);
 };
 
 function addMatching() {
